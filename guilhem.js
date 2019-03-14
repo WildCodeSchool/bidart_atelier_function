456b6ed66bc994ed6bdbbc42461c08b491c7cd5f
Guilhem Demont <guilhem.d.gd@gmail.com>
2019-03-14 15:10:21 +0100

    exercise

---
+++ guilhem.js
@@ -0,0 +1,23 @@
+function ezpz(x){
+    return x;
+}
+
+function double(string){
+    return string;
+}
+
+function multiply(num){
+    return (num*2);
+}
+
+function chaine(string){
+    return (string + '');
+}
+
+console.log(ezpz("hello world"));
+console.log(double('blabla'));
+console.log(multiply(5));
+console.log(chaine('*John*'));
+
+
+
