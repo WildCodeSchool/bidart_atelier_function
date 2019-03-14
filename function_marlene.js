e3949338369e53051f4903023b3a202b26611e1e
Marlene Angouillant <marlene.angouillant@gmail.com>
2019-03-14 15:30:25 +0100

    commit marlene function's exercise

---
+++ function_marlene.js
@@ -0,0 +1,21 @@
+console.log('Hello world')
+
+function multiply(a){
+    return a * 2;
+}
+const result1 = multiply(4);
+console.log(result1);
+
+
+function alo(a, b){
+    return a * b;
+} 
+const result = alo(5,7);
+console.log(result);
+
+
+function myChar(char) {
+    return "*" + char + "*";
+}
+const result2= myChar('Atelier sur les fonctions');
+console.log(result2);
